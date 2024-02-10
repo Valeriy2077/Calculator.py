4dbe3abadadecb5411dab1b934c01d9a749cae34
Valeriy Kovalchuk <84186810+Valeriy2077@users.noreply.github.com>
2024-02-10 17:26:14 +0300

    Change client.py, server.py

--- client.py
+++ client.py
@@ -1,25 +1,32 @@
 import socket
 
-SERVER = "127.0.0.1"
-PORT = 8000
+class CalculatorClient:
+    def run_client(self):
+        SERVER = "127.0.0.1"
+        PORT = 8000
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client:
-    try:
-        while True:
-            print("\n-Нельзя делить на 0\n-Вводите только числа\n-Введите stop,либо Ctrl+C чтобы остановить\nПример: 5+8*7")
-            inp = input("Введите выражение: ")
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as client:
+            try:
+                while True:
+                    print("\n-Нельзя делить на 0\n-Вводите только числа\n-Введите 'stop', "
+                          "либо Ctrl+C чтобы остановить\nВведите 'mem' для отображение последнего результата и 'mem [операция][число]' \nПример: 5+8*7\nmem+4")
+                    inp = input("Введите выражение: ")
 
-            if inp == "stop":
-                break
+                    if inp.lower() == "stop":
+                        break
 
-            client.sendto(inp.encode(), (SERVER, PORT))
-            result, _ = client.recvfrom(1024)
+                    client.sendto(inp.encode(), (SERVER, PORT))
+                    result, _ = client.recvfrom(1024)
 
-            if result.decode() == "division_by_zero_error":
-                print("Ошибка: Нельзя делить на ноль!")
-            elif "Некорректное выражение!" in result.decode():
-                print(f"Ошибка: {result.decode()}")
-            else:
-                print("Result:", result.decode())
-    except KeyboardInterrupt:
-        print("Клиент прерван пользователем")
+                    if result.decode() == "division_by_zero_error":
+                        print("Ошибка: Нельзя делить на ноль!")
+                    elif "Некорректное выражение!" in result.decode():
+                        print(f"Ошибка: {result.decode()}")
+                    else:
+                        print("Result:", result.decode())
+            except KeyboardInterrupt:
+                print("Клиент прерван пользователем")
+
+if __name__ == "__main__":
+    client = CalculatorClient()
+    client.run_client()
--- server.py
+++ server.py
@@ -1,57 +1,81 @@
 import socket
 import re
+from queue import Queue, PriorityQueue
 
-def calculate_result(expression):
-    if not re.match(r'^[\d\s\+\-\*/]+$', expression):
-        raise ValueError("Некорректное выражение!")
+class CalculatorServer:
+    def __init__(self):
+        self.operations_queue = Queue() 
+        self.priority_queue = PriorityQueue() 
+        self.previous_result = None
 
-    tokens = re.findall(r'(\d+|\S)', expression)
-    numbers = [int(token) for token in tokens if token.isdigit()]
-    operators = [token for token in tokens if token in {'+', '-', '*', '/'}]
+    def calculate_result(self, expression):
+        if not re.match(r'^[\d\s\+\-\*/]+$', expression):
+            raise ValueError("Некорректное выражение!")
 
-    result = numbers[0]
-    for i in range(len(operators)):
-        operator = operators[i]
-        number = numbers[i + 1]
+        tokens = re.findall(r'(\d+|\S)', expression)
+        numbers = [int(token) for token in tokens if token.isdigit()]
+        operators = [token for token in tokens if token in {'+', '-', '*', '/'}]
 
-        if operator == '+':
-            result += number
-        elif operator == '-':
-            result -= number
-        elif operator == '*':
-            result *= number
-        elif operator == '/':
-            if number == 0:
-                return "division_by_zero_error"
-            result /= number
+        result = numbers[0]
+        for i in range(len(operators)):
+            operator = operators[i]
+            number = numbers[i + 1]
 
-    return result
+            if operator == '+':
+                result += number
+            elif operator == '-':
+                result -= number
+            elif operator == '*':
+                result *= number
+            elif operator == '/':
+                if number == 0:
+                    return "division_by_zero_error"
+                result /= number
 
-LOCALHOST = "127.0.0.1"
-PORT = 8000
+        return result
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server:
-    server.bind((LOCALHOST, PORT))
-    print("Сервер запущен")
+    def process_request(self, expression):
+        try:
+            if expression.lower() == 'mem':
+                return self.previous_result
 
-    try:
-        while True:
-            data, client_address = server.recvfrom(1024)
-            print("Выражение получено", client_address)
+            
+            if 'mem' in expression.lower():
+                expression = expression.replace('mem', str(self.previous_result))
 
-            msg = data.decode()
+            result = self.calculate_result(expression)
+            self.previous_result = result
+            return result
+        except ValueError as ve:
+            return str(ve)
 
-            if msg == 'stop':
-                print("Сервер остановлен")
-                break
+    def run_server(self):
+        LOCALHOST = "127.0.0.1"
+        PORT = 8000
+
+        with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server:
+            server.bind((LOCALHOST, PORT))
+            print("Сервер запущен")
 
             try:
-                result = calculate_result(msg)
-                output = str(result)
-                server.sendto(output.encode(), client_address)
-            except ValueError as ve:
-                server.sendto(str(ve).encode(), client_address)
-    except KeyboardInterrupt:
-        print("Сервер прерван пользователем")
-    finally:
-        server.close()
+                while True:
+                    data, client_address = server.recvfrom(1024)
+                    print("Выражение получено", client_address)
+
+                    msg = data.decode()
+
+                    if msg.lower() == 'stop':
+                        print("Сервер остановлен")
+                        break
+
+                    result = self.process_request(msg)
+                    output = str(result)
+                    server.sendto(output.encode(), client_address)
+            except KeyboardInterrupt:
+                print("Сервер прерван пользователем")
+            finally:
+                server.close()
+
+if __name__ == "__main__":
+    server = CalculatorServer()
+    server.run_server()
